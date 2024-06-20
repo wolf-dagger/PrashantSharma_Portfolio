9a20acb8ad47134315a5ec77510f4c590d68c5da
wolf-dagger <demonwolfisback@gmail.com>
2024-06-20 13:55:25 +0530

    Almost done

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { InfiniteMovingCards } from "./InfiniteMovingCards";
-import { testimonials } from "@/data";
+import { companies, testimonials } from "@/data";
 
 const Clients = () => {
   return (
     <>
-      <div className="py-20" id="projects">
+      <div className="py-20" id="testimonials">
         <h1 className="heading">
           Kind Words From
           <span className="text-purple"> Previous Employers</span>
@@ -19,6 +19,14 @@ const Clients = () => {
               speed="slow"
             />
           </div>
+          {/* <div className="flex  flex-row flex-wrap items-center justify-center gap-4 md:gap-16 max-lg md:max-w-60">
+            {companies.map(({ id, img, name, nameImg }) => (
+              <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
+                <img src={img} alt={name} className="md:w-10 w-5" />
+                <img src={nameImg} alt={name} className="md:w-24 w-5 w-20" />
+              </div>
+            ))}
+          </div> */}
         </div>
       </div>
     </>
---
+++ components/Footer.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="w-full pt-20 pb-10 mb-10" id="contact">
+        <div className="flex flex-col items-center">
+          <h1 className="heading lg:max-w-[45vw]">
+            Thats all from <span className="text-purple">my side</span>. Please
+            feel free to reach out to me.
+          </h1>
+          <a href="mailto:prashantp5sharma.1998@gmail.com">
+            <MagicButton
+              title="Let's get in touch"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+        <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+          <p className="md:text-base text-sm font-light">
+            Copyright © 2024 PRASHANT SHARMA
+          </p>
+          <div className="flex items-center md:gap-3 gap-6">
+            {socialMedia.map((profile) => (
+              <div
+                key={profile.id}
+                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              >
+                <img src={profile.img} width={20} height={20} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
